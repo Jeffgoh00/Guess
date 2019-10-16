d6e19107c7457fca8272395f90020eacd7922f72
Jeff Goh <gohchinhong00@gmail.com>
2019-10-16 23:17:57 +0800

    add

--- guess.py
+++ guess.py
@@ -1,12 +1,17 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
+
 while True:
+	count += 1 # count = count + 1
 	num = int (input ("Guess Number: "))
 	if num == r:
 		print ("You guess right!")
+		print ("You have guess", count, "time")
 		break
 	elif num > r:
 		print ("Big than number ")
 	elif num < r:
 		print ("Small than number")
+	print ("You have guess", count, "time")
