d0604ddc3813800a42665e94268311f7d22a7779
Jeff Goh <gohchinhong00@gmail.com>
2019-10-16 23:23:37 +0800

    add start+end

--- guess.py
+++ guess.py
@@ -1,4 +1,7 @@
 import random
+start = int (input ("Please decide start of the answer range: "))
+end = int (input ("Please decide end of the answer range: "))
+
 
 r = random.randint(1, 100)
 count = 0
